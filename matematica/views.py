3e7281fbe0573a7c11e31a3eaff58d1cab9cc08e
Thais <thaisnviana@gmail.com>
2015-05-06 18:44:59 -0300

    main template
    main template

--- matematica/views.py
+++ matematica/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.template import Context, RequestContext
+from django.shortcuts import render, render_to_response, redirect
 
 def index(request):
-    return HttpResponse("Home page linda by victor e ana")
+	ctx = {'message' : 'Informatica aplicada ao ensino'}
+	return render(request, 'index.html', ctx)
