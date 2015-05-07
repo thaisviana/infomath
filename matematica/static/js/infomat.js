f9c0abff0a5dc6189bdf531096caf11908a9e2d5
Thais <thaisnviana@gmail.com>
2015-05-07 13:09:39 -0300

    statics
    jquery
    css
    images

---
+++ matematica/static/js/infomat.js
@@ -0,0 +1,24 @@
+$(function() {
+
+	$("#extend_add").hide();
+	$("#extend_sub").hide();
+	$("#extend_money").hide();
+	$("#extend_mcdu").hide();
+	
+	$("#add").click(function(event) {	
+		$("#extend_add").show();
+	});
+	
+	$("#sub").click(function(event) {	
+		$("#extend_sub").show();
+	});
+	
+	$("#money").click(function(event) {	
+		$("#extend_money").show();
+	});
+	
+	$("#mcdu").click(function(event) {	
+		$("#extend_mcdu").show();
+	});
+	
+});
